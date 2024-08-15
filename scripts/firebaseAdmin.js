2e388090a581123038276eed3fb7ca09f5fb7396
Bello Ibrahim <154725657+BalezDev@users.noreply.github.com>
2024-08-15 02:08:45 +0100

    adjust

--- scripts/firebaseAdmin.js
+++ scripts/firebaseAdmin.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Initialize Firebase Admin SDK
-const serviceAccountPath = path.resolve(__dirname, '../config/serviceAccountKey.json');
+const serviceAccountPath = path.resolve(__dirname, '../../servicekey/config/serviceAccountKey.json');
 const firebaseApp = initializeApp({
   credential: cert(serviceAccountPath),
   databaseURL: `https://${process.env.VITE_PROJECT_ID}.firebaseio.com`,
