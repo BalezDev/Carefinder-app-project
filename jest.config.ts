988e27e9a858edbfa422bd7afb04493625f90a62
Bello Ibrahim <154725657+BalezDev@users.noreply.github.com>
2024-08-24 15:40:14 +0100

    final crack at tests

--- jest.config.ts
+++ jest.config.ts
@@ -10,3 +10,18 @@ const config: Config = {
 };
 
 export default config;
+
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "jsdom",
+//   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+//   moduleNameMapper: {
+//     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+//   },
+
+//   globals: {
+//     "ts-jest": {
+//       tsconfig: "tsconfig.json",
+//     },
+//   },
+// };
