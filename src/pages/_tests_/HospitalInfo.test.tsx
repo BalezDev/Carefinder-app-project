e822065c97e3230a223ffe088213567a8f2136e8
Bello Ibrahim <154725657+BalezDev@users.noreply.github.com>
2024-08-24 15:09:16 +0100

    fixed import in a test file

--- src/pages/_tests_/HospitalInfo.test.tsx
+++ src/pages/_tests_/HospitalInfo.test.tsx
@@ -4,6 +4,7 @@ import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import axios from 'axios';
 import HospitalInfo from '../HospitalDetails';
+import React from 'react';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
